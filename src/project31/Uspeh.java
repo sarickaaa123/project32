ef84b8ae4a4cfe6123bb479879e6e4a166c63ce3
Sara Usevska <sarausevska42@yahoo.com>
2023-12-05 13:51:48 +0100

    first commit

---
+++ src/project31/Uspeh.java
@@ -0,0 +1,26 @@
+package project31;
+import java.util.Scanner;
+public class Uspeh {
+
+public static void metodUspeh(double bodovi) {
+ if(bodovi >=8.5) {
+  System.out.println("Odlicen");
+ }else if(bodovi >=7.5) {
+			  System.out.println("Mnogu dobar");
+ }else if(bodovi >=5.5) {
+			  System.out.println("Dobar");
+ }else  if(bodovi >=3.5) {
+ System.out.println("Dovolen");	  
+ }else {
+ System.out.println("Nedovolen");
+ }
+}
+public static void main(String[] args) {
+Scanner input = new Scanner(System.in);
+System.out.print("Vnesi bodovi od 1-10\n:");
+System.out.print("Broj na bodovi:");
+double bodovi = input.nextDouble();
+System.out.print("Za broj na bodovi + bodovi + uspehot e:");
+metodUspeh(bodovi);
+}
+}
